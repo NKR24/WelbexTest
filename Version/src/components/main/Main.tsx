132d60c4d897f8270e2d5528aff746bd0799518f
fe-dor <parashchenko.fedor@gmail.com>
2023-12-10 23:29:41 +0300

    the main blok is almost finished

--- Version/src/components/main/Main.tsx
+++ Version/src/components/main/Main.tsx
@@ -1,9 +1,44 @@
-import styles from "./main.modules.scss"
+import styles from "./main.module.scss"
 
-export default function Main(){
-  return(
+export default function Main() {
+    return (
     <>
-      <h1>HUI</h1>
+      <div class={styles.container}>
+        <div class={styles.leftPart}>
+          <div class={styles.lEmpty}></div>
+          <div class={styles.l1text}>Зарабатывайте больше</div>
+          <div class={styles.l2text}>с WELBEX</div>
+          <div class={styles.l2Empty}></div>
+          <div class={styles.l3text}>Развиваем и контролируем продажи за вас</div>
+        </div>
+        <div class={styles.rightPart}>
+          <div class={styles.rEmpty}></div>
+          <div class={styles.r1text}>
+            <div class={styles.r1PartColor}>
+              <span class={styles.r1PartWhite}>Вместе с </span>бесплатной консультацией<span class={styles.r1PartWhite}> мы дарим:</span>
+            </div>
+          </div>
+          <div class={styles.r2Empty}></div>
+          <div class={styles.r2text}>
+            <div class={styles.item1}>
+              <div class={styles.r2textTitle}>Виджеты</div>
+              <div class={styles.r2textOrdianry}>30 готовых решений</div>
+            </div>
+            <div class={styles.item2}>
+              <div class={styles.r2textTitle}>Dashboard</div>
+              <div class={styles.r2textOrdianry}>c показателями вашего бизнеса</div>
+            </div>
+            <div class={styles.item3}>
+              <div class={styles.r2textTitle}>Skype Аудит</div>
+              <div class={styles.r2textOrdianry}>отдела продаж и CRM системы</div>
+            </div>
+            <div class={styles.item4}>
+              <div class={styles.r2textTitle}>35 дней</div>
+              <div class={styles.r2textOrdianry}>использования CRM</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
--- Version/src/components/navbar/Navbar.tsx
+++ Version/src/components/navbar/Navbar.tsx
@@ -1,7 +1,6 @@
 import styles from "./navbar.module.scss"
 
 export default function Navbar(){
-  // @ts-ignore
     return(
     <>
       <div class={styles.container}>
